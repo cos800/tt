a913d5dc9fed3cb0e1fc821e17fb62325773b6d1
王道中强流 <thisky@qq.com>
2021-01-11 15:38:38 +0800

    no message

--- TT.php
+++ TT.php
@@ -8,7 +8,17 @@ class TT
     }
 
     static function deJson($str) {
-
         return json_decode($str, true);
+    }
+
+    static function setUnlimit($flush=false) {
+        set_time_limit(0) OR die('set_time_limit fail');
+        if ($flush) {
+            ob_end_flush() OR die('ob_end_flush fail');
+        }else{
+            ob_end_clean() OR die('ob_end_clean fail');
+        }
+        ob_implicit_flush();
+        header('X-Accel-Buffering: no');
     }
 }
